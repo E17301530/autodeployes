6a872c3c8394f25d89d40eeec882051bf35e953e
eduardo camino <17301530@uttt.edu.mx>
2021-03-15 13:48:31 -0600

    primeros scrips

---
+++ installCTFd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+echo "intalando CTFd.......";
+echo "actualizando apt";
+sudo apt-get update
+echo "instalando git";
+sudo apt-get install git
+mkdir CTFd
+cd CTFd
+
+echo "DEscargando repositorio de git ";
+git clone https://github.com/CTFd/CTFd.git
+echo "instalando requerimientos";
+cd  CTFd
+pip install -r requirements.txt
+echo "iniciando ...";
+python3-wsdump serve.py
+echo "";
---
+++ installFbCTF.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+echo "intalando CTFd.......";
+echo "actualizando apt";
+sudo apt-get update
+echo "instalando git";
+sudo apt-get install git
+mkdir fbctf
+cd fbctf
+
+if 
+echo "DEscargando repositorio de git ";
+git clone https://github.com/facebook/fbctf
+cd fbctf
+source ./extra/lib.sh
+quick_setup install dev
